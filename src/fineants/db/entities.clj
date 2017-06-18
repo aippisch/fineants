9a81cf9df9e0b38fd8910740ed0504c65373df8e
Andre Ippisch <ippisch@cs.uni-duesseldorf.de>
2017-06-18 13:16:12 +0200

    Add functions for retrieving data

--- src/fineants/db/entities.clj
+++ src/fineants/db/entities.clj
@@ -9,7 +9,7 @@
            (pk :id)
            (table :users)
            (database fadb)
-           (entity-fields :email :name)
+           (entity-fields :id :email :name)
            (many-to-many groups :groupmembers))
 
 (defentity groups
@@ -18,3 +18,31 @@
            (database fadb)
            (entity-fields :name)
            (many-to-many users :groupmembers))
+
+(def users-base (-> (select* users)))
+
+(def users-ordered (-> users-base
+                       (order :created)))
+
+(def get-users
+  (select users-ordered))
+
+(defn get-user-with-id
+  "get user for id"
+  [id]
+  (select users-ordered
+          (where {:id id})))
+
+(defn get-users-with-name
+  "get users for name"
+  [name]
+  (select users-ordered
+          (where {:name [like name]})))
+
+(def groups-base (-> (select* groups)))
+
+(def groups-ordered (-> groups-base
+                        (order :created)))
+
+(def get-groups
+  (select groups-ordered))
